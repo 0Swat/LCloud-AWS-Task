99ecc792dbc96366fa770d801b8ee7508ac844ba
Oskar Swat <oskar.swat@tlen.pl>
2024-10-14 18:16:10 +0200

    File list with regex

--- s3_cli.py
+++ s3_cli.py
@@ -34,6 +34,28 @@ def upload_file(local_file, s3_path_file):
     except Exception as e:
         print(f"Error occured: {e}")
 
+# 3. List an AWS buckets files that match a "filter" regex 
+def list_files_filter(regex_pattern):
+    s3_resource = boto3.resource(
+        's3',
+        aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
+        aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
+    )
+
+    bucket = s3_resource.Bucket(BUCKET_NAME)
+    compiled_pattern = re.compile(regex_pattern) 
+
+    matched_files = []
+    for obj in bucket.objects.filter(Prefix=PREFIX):
+        if compiled_pattern.search(obj.key):
+            matched_files.append(obj.key)
+            print(obj.key) 
+
+    if not matched_files:
+        print("Brak plików pasujących do wzoru.")
+
 
 if __name__ == '__main__':
     list_files()
+    upload_file('test_file.txt', 'TIE-rp/test_file.txt')
+    list_files_filter(r'.*\.txt')
