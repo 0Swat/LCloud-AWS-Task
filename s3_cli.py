0f3287e484ed965b406a26f88eba829475767192
Oskar Swat <oskar.swat@tlen.pl>
2024-10-14 18:14:55 +0200

    File upload

--- s3_cli.py
+++ s3_cli.py
@@ -27,6 +27,13 @@ def list_files():
     else:
         print("No files found.")
 
+# 2. Upload a local file to a defined location in the bucket
+def upload_file(local_file, s3_path_file):
+    try:
+        s3.upload_file(local_file, BUCKET_NAME, s3_path_file)
+    except Exception as e:
+        print(f"Error occured: {e}")
+
 
 if __name__ == '__main__':
     list_files()
